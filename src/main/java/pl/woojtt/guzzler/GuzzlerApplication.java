23d7b264e797eef176110b9e895394a0dc48fe65
woojtt <grojecwojciech@gmail.com>
2020-08-05 21:44:27 +0200

    feat: add init (#1)

---
+++ src/main/java/pl/woojtt/guzzler/GuzzlerApplication.java
@@ -0,0 +1,13 @@
+package pl.woojtt.guzzler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuzzlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuzzlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/woojtt/guzzler/GuzzlerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.woojtt.guzzler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuzzlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
